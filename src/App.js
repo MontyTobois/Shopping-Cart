f5358383b728dbde568ea39ddc135e94a2b67353
Andre-Tobois <tobois.andre@gmail.com>
2018-12-16 23:56:02 -0500

    feat: Added the savings component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import {Grid} from 'react-bootstrap';
 import Subtotal from './components/Subtotal';
+import PickupSavings from './components/PickupSavings';
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props)
       this.state ={
-        total: 1000
+        total: 1000,
+        pickupSavings: -3.85
       };
   }
 
@@ -16,6 +18,7 @@ class App extends Component {
       <Grid className="purchase-card">
         <h1>Hello world</h1>
         <Subtotal price={this.state.total.toFixed(2)} />
+        <PickupSavings price={this.state.pickupSavings} />
 
       </Grid>
     </div>);
---
+++ src/components/PickupSavings.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Row, Col, Tooltip, OverlayTrigger } from 'react-bootstrap';
+
+let styles = {
+  pickupSavings: {
+    textDecoration: 'underline'
+  },
+  totalSavings: {
+    color:'red',
+    fontWeight: 750
+  }
+}
+
+export class PickupSaving extends Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    const tooltip = (
+      <Tooltip id="tooltip">
+        <p>Pickup you order in store to cut cost and save you more money.</p>
+      </Tooltip>
+    )
+    return (
+      <Row className="show-grid">
+        <Col md={6}>
+          <OverlayTrigger placement="bottom" overlay={tooltip}>
+            <div style={styles.pickupSavings}>Pickup Savings</div>
+          </OverlayTrigger>
+        </Col>
+        <Col style={styles.totalSavings} md={6}>{`$${this.props.price}`}</Col>
+      </Row>
+    );
+  }
+}
+
+// ComponentName.propTypes = {
+//   prop: PropTypes.type.isRequired
+// }
+
+export default PickupSaving;
